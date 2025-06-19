b8322f50597f06d116c2e2676df36e47fc20dc8d
Petr Skoda <petr@mutms.org>
2025-06-19 13:07:10 +0200

    Fix Moodle 5.0.1 compatibility

--- CHANGES.md
+++ CHANGES.md
@@ -1,17 +1,7 @@
 # Changelog
 
-## mu-4.5.5-01+
+## mu-5.0.1-01
 
 Release date: xx/xx/2025
 
-* Fixed test compatibility with Moodle 5.0.
-
-## mu-4.5.5-01
-
-Release date: 09/06/2025
-
-* Added shared "Not set" string.
-* Added role_util helper.
-* Added support for user autocomplete errors.
-* Added entity details output component.
-* Improved docs and added acknowledgements.
+* Fixed compatibility with Moodle 5.0.1 release.
--- version.php
+++ version.php
@@ -30,9 +30,9 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mulib';
-$plugin->version   = 2025060900;
-$plugin->requires  = 2024100700;
+$plugin->version   = 2025061950;
+$plugin->requires  = 2025041400;
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.5-01+';
+$plugin->supported = [500, 500];
+$plugin->release   = 'mu-5.0.1-01';
 
