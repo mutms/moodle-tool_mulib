52df8c940bce4193e522cd751ac816e9a5a3aa1f
Petr Skoda <petr@mutms.org>
2025-12-03 20:49:24 +0100

    Clean up requirements

--- lang/en/tool_mulib.php
+++ lang/en/tool_mulib.php
@@ -24,9 +24,6 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['environment_64bit_ok'] = 'PHP supports 64-bit integers and dates.';
-$string['environment_64bit_required'] = 'PHP does not support 64-bit integers and dates.';
-$string['environment_mssql_unsupported'] = 'MS SQL Server database is not supported for use with MuTMS plugins.';
 $string['environment_oracle_incompatible'] = 'Oracle Database is not compatible with MuTMS plugins.';
 $string['environment_windows_unsupported'] = 'MS Windows is not supported as web server for use with MuTMS plugins. ';
 $string['extdb'] = 'External databases';
