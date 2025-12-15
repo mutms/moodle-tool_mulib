2dd5aa5f96df39594ea92e7010744956e03a08e4
Petr Skoda <petr@mutms.org>
2025-12-15 22:31:32 +0100

    Prepare release

--- version.php
+++ version.php
@@ -30,9 +30,9 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mulib';
-$plugin->version = 2025121045;
+$plugin->version = 2025121645;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'mu-4.5.8-01+';
+$plugin->release = 'mu-4.5.8-02';
