a366b2580de54507b1edb3f56c9a7562522ff71b
Petr Skoda <petr@mutms.org>
2025-12-28 18:00:21 +0100

    Add url_clipboard renderable element

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mulib';
-$plugin->version = 2025122145.01;
+$plugin->version = 2025122145.02;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
