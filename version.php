d537d601cac3f10ce6193ff74235496c96f985d0
Petr Skoda <petr@mutms.org>
2026-08-08 11:34:35 +0000

    Prepare release

--- version.php
+++ version.php
@@ -33,4 +33,4 @@ $plugin->component = 'tool_mulib';
 $plugin->version = 2026060550;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
-$plugin->release = 'v5.0.8.01+';
+$plugin->release = 'v5.0.9.01';
